58c3e2e25917a3c6ff6719e5124bebd006b77604
fatiao <me2019@aliyun.com>
2021-02-24 16:27:00 +0800

    update skiplist

--- src/educational/skiplist.hpp
+++ src/educational/skiplist.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <fmt/core.h>
 #include <functional>
 #include <list>
 #include <random>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -41,21 +43,64 @@ DefComparator<Key> *GetDefComparator() {
   return &gCmp;
 }
 
-template<typename Key>
-class Skiplist {
-  class Node;
-
+class SimpleKeyHandle {
 public:
-  Skiplist(int seed = 0);
-  ~Skiplist();
-  int GetLevel();
+  SimpleKeyHandle() : key_(777) {}
+  explicit SimpleKeyHandle(int k) : key_(k) {}
+  SimpleKeyHandle(int k, void *v) : key_(k), val_(v) {}
+  bool operator<(const SimpleKeyHandle &rhs) const { return key_ < rhs.key_; }
+  bool operator>(const SimpleKeyHandle &rhs) const { return key_ > rhs.key_; }
+  bool operator==(const SimpleKeyHandle &rhs) const { return key_ == rhs.key_; }
+  std::string ToString(bool full) const {
+    return full ? fmt::format("{} -> {}", key_, val_) : fmt::format("{}", key_);
+  }
 
 private:
-  Node* newNode(Key k, int level);
-  void findPreceeds(Key k, std::vector<Node*> &r);
+  int key_;
+  void *val_;
+};
+
+// a formatter for SimpleKeyHandle
+template<>
+struct fmt::formatter<SimpleKeyHandle> : fmt::formatter<std::string> {
+  template<typename FormatContext>
+  auto format(const SimpleKeyHandle &skh, FormatContext &ctx)
+      -> decltype(ctx.out()) {
+    return fmt::formatter<std::string>::format(skh.ToString(false), ctx);
+  }
+};
+
+//
+// TODO - `Key` should be designed as a handle
+//
+template<typename Key>
+class Skiplist {
+public:
+  class Node;
+  struct Policy {
+    static constexpr int kKeyAllowedMulti = 1 << 0;
+    Policy() : p_(0) {}
+    Policy(int p) : p_(p) {}
+    bool IsAllowMulti() { return p_ & kKeyAllowedMulti; }
+    int p_;
+  };
+  Skiplist(int seed = 0, int policy = 0);
+  ~Skiplist();
+
+  bool Insert(const Key &k);
+  bool Remove(const Key &k);
+
+  using HookOnFound = std::function<void(const Key &, Key &)>;
+  bool Search(Key &k, HookOnFound f);
+
+  void DumpAsGraphvizScatch(bool node_full_id, std::string &r);
+
+  int GetLevel();
+  Node *newNode(Key k, int level);
+  void findPreceeds(Key k, std::vector<Node *> &r);
 
 public:
-  static constexpr int kP_ = 25;
+  static constexpr int kP_ = 50;
   static constexpr int kPMax_ = 100;
   static constexpr int kMaxLvl_ = 12;
 
@@ -63,18 +108,20 @@ private:
   std::mt19937 generator_;
   std::uniform_int_distribution<int> dist_;
 
+  Comparator<Key> *cmp_;
+  Policy plc_;
+
   Node *head_;
   std::list<Node *> alloc_track_;
-  Comparator<Key> *cmp_;
-
-  int curr_lvl_; 
+  int curr_lvl_;
   // current level: [0, curr_lvl_]
 };
 
 template<typename Key>
-Skiplist<Key>::Skiplist(int seed)
-    : generator_(seed), dist_(1, kPMax_), head_(nullptr), cmp_(GetDefComparator<Key>()), curr_lvl_(0) {
-  head_ = newNode(0, kMaxLvl_);
+Skiplist<Key>::Skiplist(int seed, int policy)
+    : generator_(seed), dist_(1, kPMax_), cmp_(GetDefComparator<Key>()),
+      plc_(policy), head_(nullptr), curr_lvl_(0) {
+  head_ = newNode(Key{}, kMaxLvl_); // a default value of Key
 }
 
 template<typename Key>
@@ -87,6 +134,71 @@ Skiplist<Key>::~Skiplist() {
 }
 
 template<typename Key>
+bool Skiplist<Key>::Insert(const Key &k) {
+  // TODO - fast path for sequantial insert
+  std::vector<Node *> preceeds;
+  findPreceeds(k, preceeds);
+  assert(preceeds[0] != nullptr);
+  auto may_eq = preceeds[0]->GetNext(0);
+  if (may_eq != nullptr && may_eq->Get() == k && !plc_.IsAllowMulti()) {
+    return false;
+  }
+
+  int nlvl = GetLevel();
+  auto nnode = newNode(k, nlvl);
+
+  int ph1_ed = nlvl <= curr_lvl_ ? nlvl : curr_lvl_;
+  for (int i = ph1_ed; i >= 0; i--) {
+    assert(preceeds[i] != nullptr);
+    assert(preceeds[i]->GetLevel() >= i);
+    Node *nxt = preceeds[i]->GetNext(i);
+    nnode->SetNext(i, nxt);
+    preceeds[i]->SetNext(i, nnode);
+  }
+  if (nlvl > curr_lvl_) {
+    int ph2_st = curr_lvl_ + 1, ph2_ed = nlvl;
+    for (int i = ph2_st; i <= ph2_ed; i++) {
+      head_->SetNext(i, nnode);
+    }
+    curr_lvl_ = nlvl;
+  }
+  return true;
+}
+
+template<typename Key>
+bool Skiplist<Key>::Remove(const Key &k) {
+  return false;
+}
+
+template<typename Key>
+bool Skiplist<Key>::Search(Key &k, Skiplist<Key>::HookOnFound f) {
+  return false;
+}
+
+template<typename Key>
+void Skiplist<Key>::DumpAsGraphvizScatch(bool node_full_id, std::string &r) {
+  std::stringstream ss;
+  for (int lvl = curr_lvl_; lvl >= 0; lvl--) {
+    Node *itr = head_;
+    bool is_first = true;
+    while (itr != nullptr) {
+      std::string s = itr->ToString(node_full_id);
+
+      if (is_first) {
+        is_first = false;
+      } else {
+        ss << "->";
+      }
+      ss << s;
+
+      itr = itr->GetNext(lvl);
+    }
+    ss << "\n";
+  }
+  r = ss.str();
+}
+
+template<typename Key>
 int Skiplist<Key>::GetLevel() {
   int l = 0;
   while (dist_(generator_) < kP_ && l <= kMaxLvl_) {
@@ -96,37 +208,46 @@ int Skiplist<Key>::GetLevel() {
 }
 
 template<typename Key>
-typename Skiplist<Key>::Node* Skiplist<Key>::newNode(Key k, int level) {
+typename Skiplist<Key>::Node *Skiplist<Key>::newNode(Key k, int level) {
   Node *n = new Node(level, k);
   alloc_track_.emplace_back(n);
+  log_info("new add: k={}, lvl={}", n->Get(), n->GetLevel());
   return n;
 }
 
 template<typename Key>
-void Skiplist<Key>::findPreceeds(Key k, std::vector<Node*> &r) {
-  r.resize(curr_lvl_ + 1);
-  for (int i = 0; i < r.size(); i++) {
-    r[i] = head_;
-  }
+void Skiplist<Key>::findPreceeds(Key k, std::vector<Node *> &r) {
+  r.resize(curr_lvl_ + 1, head_);
 
-  int l = curr_lvl_;
-  Node* n = head_;
-  while (l >= 0) {
-    if (n != nullptr) {
-      Node* ln = n->GetNext(l);
-      if (ln != nullptr && (*cmp_)(ln->Get(), k) < 0) {}
+  int lvl = curr_lvl_;
+  Node *node = head_;
+  while (lvl >= 0 && node != nullptr) {
+    Node *nxt = node->GetNext(lvl);
+
+    // n      ln
+    // *   *  *
+    //     k
+    //
+    if (nxt != nullptr && (*cmp_)(k, nxt->Get()) > 0) {
+      node = nxt;
+    } else { // nxt == nullptr
+             // k < nxt->Get()
+      r[lvl] = node;
+      lvl--;
     }
   }
 }
 
 //
-// a "lvl_" node means this node have {"lvl_" + 1} next array.
+// "lvl_" means this node's topest level.
+// thus this node have an array of {"lvl_" + 1} items.
 //
 template<typename Key>
 class Skiplist<Key>::Node {
 public:
   Node(int lvl, Key k) : k_(k), lvl_(lvl) { next_.resize(lvl_ + 1, nullptr); }
   Key Get() { return k_; }
+  int GetLevel() { return lvl_; }
   // lvl: [0,lvl_ - 1]
   bool SetNext(int lvl, Node *n) {
     // assert(lvl >= 0 && lvl <= next_.size() - 1);
@@ -137,17 +258,30 @@ public:
     log_error("fatal: setnext: {}, max: {}", lvl, next_.size() - 1);
     return false;
   }
-  Node* GetNext(int lvl) {
+  Node *GetNext(int lvl) {
     if (lvl >= 0 && lvl <= next_.size() - 1) {
       return next_[lvl];
     }
     log_error("fatal: getnext: {}, max: {}", lvl, next_.size() - 1);
     return nullptr;
   }
+  std::string ToString(bool full = false) {
+    return full ? fmt::format("N{}-{:p}", k_, (void *)this)
+                : fmt::format("N{}", k_);
+  }
 
 private:
   Key k_;
   int lvl_;
   std::vector<Node *> next_;
+  // I'm prefer to use a vector than a struct hack
+  // which may look like below:
+  //    ```
+  //    struct x {
+  //       ...
+  //       SomeType node [0];
+  //    };
+  //    ```
+  //
 };
 
--- src/educational/skiplist_test.cc
+++ src/educational/skiplist_test.cc
@@ -3,6 +3,7 @@
 
 #include <gtest/gtest.h>
 #include <iostream>
+#include <string>
 #include "log.h"
 #include "skiplist.hpp"
 
@@ -14,15 +15,24 @@ TEST(skiplist, ShowTopic) {
   ShowTopic();
 }
 
-TEST(comparator, basic) {
+TEST(comparator, _int_) {
   ASSERT_TRUE((*GetDefComparator<int>())(1, 2) < 0) << "cmp(1,2) < 0";
   ASSERT_EQ((*GetDefComparator<int>())(1, 1),0) << "cmp(1,1) == 0";
   ASSERT_TRUE((*GetDefComparator<int>())(2, 1) > 0) << "cmp(2,1) > 0";
 }
 
+TEST(comparator, _string_) {
+  ASSERT_TRUE((*GetDefComparator<std::string>())("1", "2") < 0) << "cmp(1,2) < 0";
+}
+
 #include <chrono>
 #include <sstream>
 #include <fmt/core.h>
+TEST(fmt, examples) {
+  int a;
+  std::string s = fmt::format("{:p}", (void*)&a);
+  log_info("{}", s);
+}
 
 
 /*
@@ -52,6 +62,56 @@ TEST(skiplist, getlevel) {
     ss << fmt::format("{:<3d} |", i);
   }
   log_info("{}", ss.str());
+}
+
+//
+TEST(Node, tostring) {
+  Skiplist<std::string>::Node s(0, "mytest");
+  log_info("{}", s.ToString());
+}
+
+TEST(keyhandle, cmp) {
+  SimpleKeyHandle a(0, nullptr), b(1, nullptr);
+  log_info("a < b: {}", std::less<SimpleKeyHandle>()(a, b));
+  ASSERT_TRUE(std::less<SimpleKeyHandle>()(a, b));
+}
+
+TEST(keyhandle, tostring) {
+  SimpleKeyHandle a(23, (void*)0xdeadbeef);
+  log_info("{}", fmt::format("{}", a));
+}
+
+TEST(skiplist, insert5) {
+  Skiplist<SimpleKeyHandle> skl;
+  int* xp = (int*)0x0;
+  skl.Insert(SimpleKeyHandle(10, xp));
+  skl.Insert(SimpleKeyHandle(20, xp++));
+  skl.Insert(SimpleKeyHandle(40, xp++));
+  skl.Insert(SimpleKeyHandle(30, xp++));
+  skl.Insert(SimpleKeyHandle(50, xp++));
+
+  std::string s;
+  skl.DumpAsGraphvizScatch(false, s);
+  log_info("after insertion:");
+  log_info("{}", s);
+}
+
+TEST(skiplist, insertx) {
+  Skiplist<SimpleKeyHandle> skl;
+  int* xp = (int*)0x0;
+  
+  std::mt19937 generator(25);
+  std::uniform_int_distribution<int> dist(10, 200);
+
+  int max = 15;
+  for (int i = 0; i < max; i++) {
+    int a = dist(generator);
+    skl.Insert(SimpleKeyHandle(a, xp++));
+  }
+  std::string s;
+  skl.DumpAsGraphvizScatch(false, s);
+  log_info("after insertion:");
+  log_info("{}", s);
 }
 
 int main(int argc, char **argv) {
